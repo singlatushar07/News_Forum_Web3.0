b5aac384f8369781ea974f667d8a8c296d4a5940
Tushar Singla <singlatushar07@gmail.com>
2023-02-12 22:11:47 +0530

    Added basic functions for the forum

--- server/contracts/NewsForum.sol
+++ server/contracts/NewsForum.sol
@@ -4,54 +4,80 @@ pragma solidity ^0.8.9;
 import "hardhat/console.sol";
 
 contract NewsForumContract {
-    Article[] public articles;
+    address owner;
+
     User[] public users;
+    mapping(address => uint256) addressToUserId;
+    
+    Article[] public articles;
     mapping(uint256 => address) articleToOwner;
 
-    event AddArticle(address recipent, uint tweetid);
+    modifier OnlyOwner {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
 
     struct Article {
         uint id;
         string title;
         string content;
         address author;
-        uint256 timestamp;
+        bool valid;
     }
 
     struct User {
         string name;
         string email;
-        uint256 timestamp;
+        address id;
+        bool valid;
+    }
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function addNewUser(string memory _name, string memory _email, address _wallet) external OnlyOwner {
+        users.push(User(_name, _email, _wallet, true));
+        addressToUserId[_wallet] = users.length - 1;
+    }
+
+    function updateUser(string memory _name, string memory _email) external {
+        require(users[addressToUserId[msg.sender]].valid == true, "You must be registered to update your profile");
+
+        users[addressToUserId[msg.sender]].name = _name;
+        users[addressToUserId[msg.sender]].email = _email;
     }
 
     function addArticle(string memory _title, string memory _content) external {
+        require(users[addressToUserId[msg.sender]].valid == true, "You must be registered to add an article");
+        
         uint id = articles.length;
-        articles.push(Article(id, _title, _content, msg.sender, block.timestamp));
+        articles.push(Article(id, _title, _content, msg.sender, true));
         articleToOwner[id] = msg.sender;
-        emit AddArticle(msg.sender, id);
     }
 
-    function getAllArticles () external view returns (Article[] memory) {
-        return articles;
-    }
+    function updateArticle(uint articleId, string memory _title, string memory _content) external {
+        require(users[addressToUserId[msg.sender]].valid == true, "You must be registered to update an article");
+        require(articleToOwner[articleId] == msg.sender, "You must be the author of the article to update it");
+        require(articles[articleId].valid == true, "Article does not exist");
 
-    function getMyArticles () external view returns (Article[] memory) {
-        Article[] memory myArticles = new Article[](articles.length);
-        uint counter = 0;
-        for (uint i = 0; i < articles.length; i++) {
-            if (articleToOwner[i] == msg.sender) {
-                myArticles[counter] = articles[i];
-                counter++;
-            }
-        }
-        return myArticles;
+        articles[articleId].title = _title;
+        articles[articleId].content = _content;
     }
 
     function getArticleById (uint _id) external view returns (Article memory) {
         return articles[_id];
     }
 
-    function addNewUser(string memory _name, string memory _email) external {
-        users.push(User(_name, _email, block.timestamp));
+    function getMyArticles () external view returns (Article[] memory) {
+        Article[] memory myArticles = new Article[](articles.length);
+        uint counter = 0;
+        for (uint i = 0; i < articles.length; i++) {
+            if (articles[i].author == msg.sender) {
+                myArticles[counter] = articles[i];
+                counter++;
+            }
+        }
+        return myArticles;
     }
 }
