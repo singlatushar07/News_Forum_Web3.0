7b00180452ddaf48795d133ff34f8e7adcd1a555
Tushar Singla <singlatushar07@gmail.com>
2023-03-01 16:14:41 +0530

    Added some code for validating an article

--- server/contracts/NewsForum.sol
+++ server/contracts/NewsForum.sol
@@ -3,7 +3,12 @@ pragma solidity ^0.8.9;
 
 import "hardhat/console.sol";
 
+
 contract NewsForumContract {
+    uint256 public constant VALIDATIONS_REQUIRED = 10;
+    uint256 public constant VALIDATION_REWARD = 10;
+    uint256 public constant NUMBER_OF_VALIDATORS = 1;
+
     address owner;
 
     User[] public users;
@@ -12,10 +17,22 @@ contract NewsForumContract {
     Article[] public articles;
     mapping(uint256 => address) articleToOwner;
 
+    mapping (address => bool) validators;
+
     modifier OnlyOwner {
         require(msg.sender == owner, "Only owner can call this function");
         _;
     }
+    modifier OnlyRegistered {
+        require(users[addressToUserId[msg.sender]].valid == true, "You must be registered to perform the action");
+        _;
+    }
+    modifier OnlyValidator {
+        require(validators[msg.sender] == true, "You must be a validator to perform the action");
+        _;
+    }
+
+    event ArticleValidated(uint256 articleId);
 
     struct Article {
         uint id;
@@ -24,6 +41,7 @@ contract NewsForumContract {
         address author;
         address[] upvotes;
         address[] downvotes;
+        address[] validators;
         bool isValidated;
         bool valid;
         uint timestamp;
@@ -46,24 +64,23 @@ contract NewsForumContract {
         addressToUserId[_wallet] = users.length - 1;
     }
 
-    function updateUser(string memory _name, string memory _email) external {
-        require(users[addressToUserId[msg.sender]].valid == true, "You must be registered to update your profile");
-
+    function updateUser(string memory _name, string memory _email) external OnlyRegistered {
         users[addressToUserId[msg.sender]].name = _name;
         users[addressToUserId[msg.sender]].email = _email;
         users[addressToUserId[msg.sender]].timestamp = block.timestamp;
     }
 
-    function addArticle(string memory _title, string memory _content) external {
-        require(users[addressToUserId[msg.sender]].valid == true, "You must be registered to add an article");
-        
+    function updateValidators() external OnlyOwner {
+        // Logic to update validators
+    }
+
+    function addArticle(string memory _title, string memory _content) external OnlyRegistered {
         uint id = articles.length;
-        articles.push(Article(id, _title, _content, msg.sender, new address[](0), new address[](0), false, true, block.timestamp));
+        articles.push(Article(id, _title, _content, msg.sender, new address[](0), new address[](0), new address[](0), false, true, block.timestamp));
         articleToOwner[id] = msg.sender;
     }
 
-    function updateArticle(uint articleId, string memory _title, string memory _content) external {
-        require(users[addressToUserId[msg.sender]].valid == true, "You must be registered to update an article");
+    function updateArticle(uint articleId, string memory _title, string memory _content) external OnlyRegistered {
         require(articleToOwner[articleId] == msg.sender, "You must be the author of the article to update it");
         require(articles[articleId].valid == true, "Article does not exist");
 
@@ -72,8 +89,7 @@ contract NewsForumContract {
         articles[articleId].timestamp = block.timestamp;
     }
 
-    function upvoteArticle(uint articleId) external {
-        require(users[addressToUserId[msg.sender]].valid == true, "You must be registered to upvote an article");
+    function upvoteArticle(uint articleId) external OnlyRegistered {
         require(articles[articleId].valid == true, "Article does not exist");
 
         for (uint i = 0; i < articles[articleId].upvotes.length; i++) {
@@ -92,8 +108,7 @@ contract NewsForumContract {
         articles[articleId].upvotes.push(msg.sender);
     }
 
-    function downvoteArticle(uint articleId) external {
-        require(users[addressToUserId[msg.sender]].valid == true, "You must be registered to downvote an article");
+    function downvoteArticle(uint articleId) external OnlyRegistered {
         require(articles[articleId].valid == true, "Article does not exist");
 
         for (uint i = 0; i < articles[articleId].downvotes.length; i++) {
@@ -112,11 +127,12 @@ contract NewsForumContract {
         articles[articleId].downvotes.push(msg.sender);
     }
 
-    function getArticleById (uint _id) external view returns (Article memory) {
-        return articles[_id];
+    function getArticleById (uint articleId) external view returns (Article memory) {
+        require(articles[articleId].valid == true, "Article does not exist");
+        return articles[articleId];
     }
 
-    function getMyArticles () external view returns (Article[] memory) {
+    function getMyArticles () external OnlyRegistered view returns (Article[] memory) {
         Article[] memory myArticles = new Article[](articles.length);
         uint counter = 0;
         for (uint i = 0; i < articles.length; i++) {
@@ -146,5 +162,35 @@ contract NewsForumContract {
             }
         }
         return allArticles;
+    }
+
+    function getAllArticlesForValidation () external view OnlyValidator returns (Article[] memory) {
+        Article[] memory allArticles = new Article[](articles.length);
+        uint counter = 0;
+        for (uint i = 0; i < articles.length; i++) {
+            if (articles[i].isValidated == false) {
+                allArticles[counter] = articles[i];
+                counter++;
+            }
+        }
+        return allArticles;
+    }
+
+    function validateArticle(uint articleId) external OnlyRegistered OnlyValidator {
+        require(articles[articleId].valid == true, "Article does not exist");
+        require(articles[articleId].isValidated == false, "Article has already been validated");
+        
+        for (uint i = 0; i < articles[articleId].validators.length; i++) {
+            if (articles[articleId].validators[i] == msg.sender) {
+                revert("You have already validated this article");
+            }
+        }
+        
+        articles[articleId].validators.push(msg.sender);
+
+        if(articles[articleId].validators.length >= NUMBER_OF_VALIDATORS/2) {
+            articles[articleId].isValidated = true;
+            emit ArticleValidated(articleId);
+        }
     }
 }
