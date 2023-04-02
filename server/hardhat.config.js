3ca5615f44e08c4eecde0ec89de5e61ea7dab887
HarshMethwani <95287106+HarshMethwani@users.noreply.github.com>
2023-04-02 14:19:44 +0530

    Some changes in Config

--- server/hardhat.config.js
+++ server/hardhat.config.js
@@ -1,6 +1,21 @@
 require("@nomicfoundation/hardhat-toolbox");
+<<<<<<< Updated upstream
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+=======
+require("dotenv").config();
+
+const GOERLI_URL = process.env.GOERLI_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+module.exports = {
+  solidity: "0.8.17",
+  networks:{
+    goerli:{
+      url:GOERLI_URL,
+      accounts:[PRIVATE_KEY]
+    }
+  }
+>>>>>>> Stashed changes
 };
